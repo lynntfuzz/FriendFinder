dd57853b9a1497bbb8e42b4b6fdf7cd8bbeaf3a1
Lynn Thompson <lynninla@earthlink.net>
2018-12-06 22:03:48 -0800

    Removed extraneous text, console.logs, and added more data

--- data/friends.js
+++ data/friends.js
@@ -1,8 +1,8 @@
-var friends = [
+[
     {
-        name:"Santa Claus",
-        photo:"https://amp.thisisinsider.com/images/5a282a7e3339b037278b45b5-1536-1152.jpg",
-        scores:[
+    name: "Santa Claus",
+    photo: "https://amp.thisisinsider.com/images/5a282a7e3339b037278b45b5-1536-1152.jpg",
+    scores: [
             3,
             4,
             1,
@@ -16,9 +16,9 @@ var friends = [
         ]
     },
     {
-        name:"Adam Levine",
-        photo:"https://assets.capitalfm.com/2012/26/adam-levine--1340705880-view-1.jpg",
-        scores:[
+    name: "Adam Levine",
+    photo: "https://assets.capitalfm.com/2012/26/adam-levine--1340705880-view-1.jpg",
+    scores: [
             3,
             5,
             5,
@@ -32,9 +32,9 @@ var friends = [
         ]
     },
     {
-        name:"Anna Louise Scott",
-        photo:"https://image.freepik.com/free-photo/pretty-woman-looking-at-camera_23-2147767413.jpg",
-        scores:[
+    name: "Anna Louise Scott",
+    photo: "https://image.freepik.com/free-photo/pretty-woman-looking-at-camera_23-2147767413.jpg",
+    scores: [
             5,
             1,
             1,
@@ -46,9 +46,42 @@ var friends = [
             4,
             1
         ]
+    },
+    {
+    name: "Lynn Thompson",
+    photo: "https://scontent-any2-1.xx.fbcdn.net/v/t1.0-1/p320x320/38894206_10215501908314250_1185399141061099520_n.jpg?_nc_cat=104&_nc_ht=scontent-any2-1.xx&oh=756bdac96b5dfe3784dc5165c2aa5526&oe=5CA8A7F9",
+    scores: [
+            4,
+            1,
+            4,
+            5,
+            5,
+            5,
+            5,
+            3,
+            4,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Paul Thompson",
+        photo: "https://www.keckmedicine.org/wp-content/uploads/2013/10/Unknown-3.jpeg",
+        "scores": [
+            5,
+            1,
+            1,
+            5,
+            5,
+            5,
+            5,
+            3,
+            1,
+            3,
+            5
+        ]
     }
-
-];
+    ]
 
 module.exports = friends;
   
